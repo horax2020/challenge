7194151856a7e92b8bb79380e3222768c59ce787
Horax2000 <horax2000@gmail.com>
2021-05-06 12:13:30 -0500

    Fixed Asserts

--- src/test/java/com/cucumberBaby/POM/Factories/AmazonChallenge/EchoPageAmzPOM.java
+++ src/test/java/com/cucumberBaby/POM/Factories/AmazonChallenge/EchoPageAmzPOM.java
@@ -10,11 +10,12 @@ public class EchoPageAmzPOM extends mapBasePOM {
     public boolean findElementFromList(List<WebElement> elements, String lookFor){
 
         elements.forEach((e) ->
-                System.out.println(e.getText().toString()));
+                System.out.println("Log:Looking for:"+ e.getText().toString()+ " comparing " + lookFor));
 
         for (int idx=0; idx < elements.toArray().length; idx++){
-            if (elements.get(idx).toString().equals(lookFor)) {
-                System.out.println(elements.get(idx).getText().toString());
+            System.out.println("LOG:Comparing:"+idx+" "+elements.get(idx).getText());
+            if (elements.get(idx).getText().equals(lookFor)) {
+                System.out.println("LOG:FOUND:"+elements.get(idx).getText());
                 return true;
             }
         }
@@ -24,10 +25,16 @@ public class EchoPageAmzPOM extends mapBasePOM {
 
     public boolean validateElements(){
         //Getting Started, Wi-Fi and Bluetooth, Device Software and Hardware, TroubleShooting
+
+        System.out.println("LOG:Find Topic:Getting Started"+findElementFromList(echoPageAmzMap.topicListElements, "Getting Started"));
+        System.out.println("LOG:Find Topic:Wi-Fi and Bluetooth"+findElementFromList(echoPageAmzMap.topicListElements, "Wi-Fi and Bluetooth"));
+        System.out.println("LOG:Find Topic:Device Software..."+findElementFromList(echoPageAmzMap.topicListElements, "Device Software and Hardware"));
+        System.out.println("LOG:Find Topic:Troubleshooting"+findElementFromList(echoPageAmzMap.topicListElements, "Troubleshooting"));
+
         if (findElementFromList(echoPageAmzMap.topicListElements, "Getting Started") &&
         findElementFromList(echoPageAmzMap.topicListElements, "Wi-Fi and Bluetooth") &&
         findElementFromList(echoPageAmzMap.topicListElements, "Device Software and Hardware") &&
-        findElementFromList(echoPageAmzMap.topicListElements, "TroubleShooting")) return true;
+        findElementFromList(echoPageAmzMap.topicListElements, "Troubleshooting")) return true;
 
 
         return false;
